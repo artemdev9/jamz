7b6943724bfbadeb7191061bfe7eb9359a127e9a
Artem <dumchevartem@gmail.com>
2023-11-10 09:15:34 +0000

    Added SignIn Component

--- src/components/SingIn.js
+++ src/components/SingIn.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Spotify from "../utilities/Spotify";
 import style from "../styles/SignIn.module.css";
 
