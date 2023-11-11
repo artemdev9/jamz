deb857abab93d25b9db9f34c2e2356ff734ad399
Artem <dumchevartem@gmail.com>
2023-11-11 15:23:11 +0000

    Added CSS

--- src/components/Information.js
+++ src/components/Information.js
@@ -5,10 +5,14 @@ function Information() {
   return (
     <div className={style.Information_container}>
       <h3>
-        This website allows you to search for songs, add them to a playlist and
-        save the playlist to your spotify account. You might want to use the
-        search suggestions as well to make the search process easier. You can
-        drag and use the mouse wheel to scroll thought the suggestions.
+        This website allows you:
+        <h3 />
+        <li>to search for songs</li>
+        <li>add them to a playlist</li>
+        <li>save the playlist to your spotify account</li>
+        You might want to use the search suggestions as well to make the search
+        process easier. You can drag and use the mouse wheel to scroll thought
+        the suggestions.
       </h3>
       <h2 className={style.Information_container_cta}>
         Go ahead and search for a song...
